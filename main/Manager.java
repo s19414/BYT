2048411ea6b3ce42aac2f306c90cbb69033e82c4
s20179 <96303199+s20179@users.noreply.github.com>
2021-12-17 17:16:52 +0100

    Update Manager.java

--- main/Manager.java
+++ main/Manager.java
@@ -1,5 +1,14 @@
 package main;
 
-public class Manager {
+public class Manager extends Employee {
+
+    Manager manager = new Manager();
+
+    public Manager() {
+    }
+
+    private String resolveIssue(){
+        return "Issue has been resolved";
+    }
 
 }
