9eac628cf2fcdb36d7b20f7d4addaf34686653d8
s21105 <80321755+s21105@users.noreply.github.com>
2021-12-17 17:29:38 +0100

    Update AirportWorker.java

--- main/AirportWorker.java
+++ main/AirportWorker.java
@@ -1,5 +1,12 @@
 package main;
 
 public class AirportWorker extends Employee{
+    AirportWorker airportWorker = new AirportWorker();
 
+    public AirportWorker() {
+    }
+
+    private String flagAnIssue(){
+        return "Issue has been flagged";
+    }
 }
