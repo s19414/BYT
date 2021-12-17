dc5eeb9035d9c543c0bb33cbdc498183f8b5c9cf
s21105 <80321755+s21105@users.noreply.github.com>
2021-12-17 17:08:04 +0100

    Update Airline.java

--- main/Airline.java
+++ main/Airline.java
@@ -1,20 +1,35 @@
 package main;
 
 public class Airline {
-  public String name;
-  public String address;
-  
-  public AirPlane(String name, String address){
-        this.name=name;
+    public String name;
+    public String address;
+
+    public Airline(String name, String address) {
+        this.name = name;
         this.address = address;
-    } 
-  
-  public String getname() {
+    }
+
+    public String getName() {
         return name;
     }
 
-   public String getAddress() {
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAddress() {
         return address;
     }
 
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    @Override
+    public String toString() {
+        return "Airline{" +
+                "name='" + name + '\'' +
+                ", address='" + address + '\'' +
+                '}';
+    }
 }
