9166987f2c663568fdcf426311a0b86c366d370e
s20179 <96303199+s20179@users.noreply.github.com>
2021-12-17 17:05:53 +0100

    Update Luggage.java

--- main/Luggage.java
+++ main/Luggage.java
@@ -2,4 +2,48 @@ package main;
 
 public class Luggage {
 
+    private int idLuggage;;
+    private String category;
+    private double weight;
+    private double price;
+
+    public Luggage(int idLuggage, String category, double weight, double price) {
+        this.idLuggage = idLuggage;
+        this.category = category;
+        this.weight = weight;
+        this.price = price;
+    }
+
+    public int getIdLuggage() {
+        return idLuggage;
+    }
+
+    public void setIdLuggage(int idLuggage) {
+        this.idLuggage = idLuggage;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+    public double getWeight() {
+        return weight;
+    }
+
+    public void setWeight(double weight) {
+        this.weight = weight;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
 }
