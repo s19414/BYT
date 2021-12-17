f22997db940f25be38d6826e24c6a092ba22c4b6
s20179 <96303199+s20179@users.noreply.github.com>
2021-12-17 17:32:17 +0100

    Update Passenger.java

--- main/Passenger.java
+++ main/Passenger.java
@@ -1,5 +1,40 @@
 package main;
 
+import java.util.UUID;
+
 public class Passenger {
 
+    private String firstName;
+    private String lastName;
+    private UUID passengerId = UUID.randomUUID();
+
+    public Passenger(String firstName, String lastName, UUID passengerId) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.passengerId = passengerId;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public UUID getPassengerId() {
+        return passengerId;
+    }
+
+    public void setPassengerId(UUID passengerId) {
+        this.passengerId = passengerId;
+    }
 }
