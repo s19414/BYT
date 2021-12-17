1a30e1b3f3b0d9eec42aa6c7f5203cd62b850341
s21105 <80321755+s21105@users.noreply.github.com>
2021-12-17 16:54:46 +0100

    Update Airline.java

--- main/Airline.java
+++ main/Airline.java
@@ -1,5 +1,20 @@
 package main;
 
 public class Airline {
+  public String name;
+  public String address;
+  
+  public AirPlane(String name, String address){
+        this.name=name;
+        this.address = address;
+    } 
+  
+  public String getname() {
+        return name;
+    }
+
+   public String getAddress() {
+        return address;
+    }
 
 }
