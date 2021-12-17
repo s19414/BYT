83e64321a7f171d029dde819e42f00518f07c334
s20179 <96303199+s20179@users.noreply.github.com>
2021-12-17 17:22:06 +0100

    Update Luggage.java

--- main/Luggage.java
+++ main/Luggage.java
@@ -5,7 +5,7 @@ public class Luggage {
     private int idLuggage;;
     private String category;
     private double weight;
-    public double price;
+    private double price;
 
     public Luggage(int idLuggage, String category, double weight, double price) {
         this.idLuggage = idLuggage;
