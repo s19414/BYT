e53fbae6fba59ff7fb2e46ff0135da2f0299a8e5
s19119 <80397351+s19119@users.noreply.github.com>
2021-12-18 17:24:45 +0100

    Update AirlineTest.java

--- test/AirlineTest.java
+++ test/AirlineTest.java
@@ -1,12 +1,46 @@
 package test;
 
+import static org.junit.Assert.assertEquals;
+
 import org.junit.Before;
+import org.junit.Test;
+
+import main.Airline;
+import main.Airplane;
 
 public class AirlineTest {
 	
+	Airline airline;
+	Airplane airplane;
+	
 	@Before
 	public void setUp() {
-		
+		airline = new Airline("Test Airline", "Poland");
+		airplane = new Airplane("Test1", 1, 1, 200, 30000);
 	}
+	
+	@Test
+	public void testGetName() {
+		assertEquals("Test Airline", airline.getName());
+	}
+	
+	@Test
+	public void testSetName() {
+		airline.setName("New Test Airline");
+		assertEquals("New Test Airline", airline.getName());
+	}
+	
+	@Test
+	public void testGetAddress() {
+		assertEquals("Poland", airline.getAddress());
+	}
+	
+	@Test
+	public void testSetAddress() {
+		airline.setAddress("Warsaw");
+		assertEquals("Warsaw", airline.getAddress());
+	}
+	
+	//test association
 
 }
