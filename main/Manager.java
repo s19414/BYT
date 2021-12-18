4c95426fb8ff4bea123e96282b91a358cea3840b
s19119 <80397351+s19119@users.noreply.github.com>
2021-12-18 16:45:44 +0100

    Update Manager.java

--- main/Manager.java
+++ main/Manager.java
@@ -1,10 +1,26 @@
 package main;
 
+import java.util.ArrayList;
+
 public class Manager extends Employee {
+    //association attributes
+	private ArrayList<Issue> issues;
 
-    Manager manager = new Manager();
-
-    public Manager() {
+    public Manager(String firstName, String lastName) {
+    	super(firstName,lastName);
+    	issues = new ArrayList<>();
+    }
+    
+    public ArrayList<Issue> getIssues(){
+    	ArrayList<Issue> copy = new ArrayList<>(issues);
+    	return copy;
+    }
+    
+    public void setIssues(ArrayList<Issue> issues) {
+    	if(issues == null) {
+    		throw new NullPointerException("Issue cannot be null");
+    	} 
+    	this.issues = new ArrayList<>(issues);
     }
 
     private String resolveIssue(){
