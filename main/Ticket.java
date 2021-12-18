6c0e6c9e365c873b9ffb3126a5071a6a31a82d33
s19119 <80397351+s19119@users.noreply.github.com>
2021-12-18 16:46:24 +0100

    Update Ticket.java

--- main/Ticket.java
+++ main/Ticket.java
@@ -1,23 +1,62 @@
 package main;
 
-public class Ticket {
+import java.util.ArrayList;
 
+public class Ticket {
+	enum Class{
+		ECONOMY,
+		BUSINESS,
+		FIRST
+	}
+	//constructor attributes
+    private Class _class;
     private int seatNummber;
     private String travelDocument;
     private double price;
-    private double luggagePenalty;
     private int maxLuggage;
+    
+    //derived attributes
+    private double luggagePenalty;
+    
+    //association attributes
+    private Flight flight;
+    private Passenger passenger;
 
-    public Ticket(int seatNummber, String travelDocument, double price, double luggagePenalty, int maxLuggage) {
-        this.seatNummber = seatNummber;
+    public Ticket(Class _class, int seatNummber, String travelDocument, double price, double luggagePenalty, int maxLuggage, Flight flight, Passenger passenger) {
+        if(_class == null) {
+        	throw new NullPointerException("Class cannot be null");
+        }
+    	this.seatNummber = seatNummber;
+    	if(travelDocument == null || travelDocument.equals("")) {
+    		throw new NullPointerException("Travel document cannot be null");
+    	}
         this.travelDocument = travelDocument;
         this.price = price;
-        this.luggagePenalty = luggagePenalty;
         this.maxLuggage = maxLuggage;
+        calculateLuggagePenalty();
+        if(flight == null) {
+        	throw new NullPointerException("Flight cannot be null");
+        }
+        this.flight = flight;
+        if(passenger == null) {
+        	throw new NullPointerException("Passenger cannot be null");
+        }
+        this.passenger = passenger;
     }
 
-    private String checkInPassanger(){
+    public String checkInPassanger(){
         return "Passenger checked in";
+    }
+    
+    public Class getClassOfTicket() {
+    	return _class;
+    }
+    
+    public void setClassOfTicket(Class _class) {
+    	if(_class == null) {
+    		throw new NullPointerException("Class cannot be null");
+    	}
+    	this._class = _class;
     }
 
     public int getSeatNummber() {
@@ -33,6 +72,9 @@ public class Ticket {
     }
 
     public void setTravelDocument(String travelDocument) {
+    	if(travelDocument == null || travelDocument.equals("")) {
+    		throw new NullPointerException("Travel document cannot be null");
+    	}
         this.travelDocument = travelDocument;
     }
 
@@ -48,15 +90,48 @@ public class Ticket {
         return luggagePenalty;
     }
 
-    public void setLuggagePenalty(double luggagePenalty) {
-        this.luggagePenalty = luggagePenalty;
-    }
-
     public int getMaxLuggage() {
         return maxLuggage;
     }
 
     public void setMaxLuggage(int maxLuggage) {
         this.maxLuggage = maxLuggage;
+        calculateLuggagePenalty();
     }
+    
+    public Flight getFlight() {
+    	return flight;
+    }
+    
+    public void setFlight(Flight flight) {
+    	 if(flight == null) {
+    		 throw new NullPointerException("Flight cannot be null");
+         }
+         this.flight = flight;
+    }
+    
+    public Passenger getPassenger() {
+    	return passenger;
+    }
+    
+    public void setPassenger(Passenger passenger) {
+        if(passenger == null) {
+        	throw new NullPointerException("Passenger cannot be null");
+        }
+        this.passenger = passenger;
+        calculateLuggagePenalty();
+    	
+    }
+    //helper method
+    private void calculateLuggagePenalty() {
+		luggagePenalty = 0;
+		ArrayList<Luggage> luggageList = passenger.getLuggages();
+		int size = luggageList.size();
+		if(size > maxLuggage) {
+			for(int i = size - 1; i >= maxLuggage; i++) {
+				Luggage luggage = luggageList.get(i);
+				luggagePenalty+=luggage.getWeight()*10;
+			}
+		}
+	}
 }
