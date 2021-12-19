bd598431f795bf50969c094be38677da42296446
s19119 <80397351+s19119@users.noreply.github.com>
2021-12-19 23:02:01 +0100

    Update PassengerTest.java

--- test/PassengerTest.java
+++ test/PassengerTest.java
@@ -1,5 +1,8 @@
 package test;
 
+import main.Airline;
+import main.Airplane;
+import main.Flight;
 import main.Luggage;
 import main.Passenger;
 import main.Ticket;
@@ -7,26 +10,40 @@ import org.junit.Before;
 import org.junit.Test;
 
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.UUID;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertThrows;
 
 public class PassengerTest {
 
 	Passenger passenger;
 	Luggage luggage;
 	Ticket ticket;
+	Airline airline;
+	Airplane airplane;
+	Flight flight;
+	UUID uuid;
 	ArrayList<Luggage> luggageList;
 	ArrayList<Ticket> ticketList;
 	
 	@Before
 	public void setUp() {
+		uuid = UUID.randomUUID();
 		passenger = new Passenger("Paul","Bloom");
 		luggage = new Luggage(25, Luggage.Category.HAND,18.3,38,passenger);
-		ticket = new Ticket(Ticket.TicketClass.ECONOMY,36,"Passport",1200,0,2, ticket.getFlight(), passenger);
+		airline = new Airline("Finnair", "Finland");
+		airplane = new Airplane("Boeing666", 444, 13, 1, 0, airline);
+		flight = new Flight("FIN007",new Date(2000), new Date(2500),new Date(4000),new Date(20000),"London Heathrow", "New York JFK", 2,airplane);
+		ticket = new Ticket(Ticket.TicketClass.ECONOMY,36,"Passport",1200,0,2, flight, passenger);
+		luggageList = new ArrayList<>();
+		ticketList = new ArrayList<>();
 		luggageList.add(luggage);
 		ticketList.add(ticket);
-
+		passenger.setLuggages(luggageList);
+		passenger.setTickets(ticketList);
+		passenger.setPassengerId(uuid);
 	}
 
 	@Test
@@ -54,14 +71,14 @@ public class PassengerTest {
 
 	@Test
 	public void testGetPassengerId(){
-		assertEquals(passenger,passenger.getPassengerId());
+		assertEquals(uuid,passenger.getPassengerId());
 	}
 
 	@Test
 	public void testSetPassengerId(){
-		UUID uuid = UUID.randomUUID();
-		passenger.setPassengerId(uuid);
-		assertEquals(uuid,passenger.getPassengerId());
+		UUID uuid2 = UUID.randomUUID();
+		passenger.setPassengerId(uuid2);
+		assertEquals(uuid2,passenger.getPassengerId());
 		assertThrows(NullPointerException.class,()->{passenger.setPassengerId(null);});
 	}
 
@@ -76,7 +93,7 @@ public class PassengerTest {
 		luggages.add(luggage);
 		passenger.setLuggages(luggages);
 		assertEquals(luggages,passenger.getLuggages());
-		assertThrows(NullPointerException.class,()->{passenger.setLuggages(luggages);});
+		assertThrows(NullPointerException.class,()->{passenger.setLuggages(null);});
 	}
 
 	@Test
@@ -90,7 +107,7 @@ public class PassengerTest {
 		tickets.add(ticket);
 		passenger.setTickets(tickets);
 		assertEquals(tickets,passenger.getTickets());
-		assertThrows(NullPointerException.class,()->{passenger.setTickets(tickets);});
+		assertThrows(NullPointerException.class,()->{passenger.setTickets(null);});
 	}
 
 }
