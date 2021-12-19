3cb0ce1e5b55e06e11abd0f8cfb37e9bc93a941e
s19119 <80397351+s19119@users.noreply.github.com>
2021-12-19 13:00:00 +0100

    Update AirplaneTest.java

--- test/AirplaneTest.java
+++ test/AirplaneTest.java
@@ -1,23 +1,39 @@
 package test;
 
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertThrows;
+
+import java.sql.Date;
+import java.time.LocalDate;
+import java.util.ArrayList;
+
 import org.junit.Before;
 import org.junit.Test;
-import static org.junit.Assert.assertEquals;
 
 import main.Airline;
 import main.Airplane;
+import main.Flight;
 
 public class AirplaneTest {
 	
 	Airplane airplane;
 	Airline airline;
 	Airline airline2;
+	Flight flight1;
+	Flight flight2;
+	ArrayList<Flight> flightList;
 	
 	@Before
 	public void setUp() {
 		airline = new Airline("Beelzebub Airlines", "Hell");
 		airline2 = new Airline("HerpyDerpHerp", "Derp");
 		airplane = new Airplane("Boeing666", 444, 13, 1, 0, airline);
+		flight1 = new Flight("BA007",Date.valueOf(LocalDate.now()), Date.valueOf(LocalDate.now()),Date.valueOf(LocalDate.now()),Date.valueOf(LocalDate.now()),"London Heathrow", "New York JFK", 2);
+		flight2 = new Flight("AA201",Date.valueOf(LocalDate.now()), Date.valueOf(LocalDate.now()),Date.valueOf(LocalDate.now()),Date.valueOf(LocalDate.now()),"London Heathrow", "New York JFK", 2);
+		flightList = new ArrayList<>();
+		flightList.add(flight1);
+		flightList.add(flight2);
+		airplane.setFlightList(flightList);
 	}
 	
 	@Test
@@ -29,6 +45,7 @@ public class AirplaneTest {
 	public void testSetName() {
 		airplane.setName("Boeing444");
 		assertEquals("Boeing444", airplane.getName());
+		assertThrows(NullPointerException.class, () -> {airplane.setName(null);});
 	}
 	
 	@Test
@@ -54,14 +71,14 @@ public class AirplaneTest {
 	}
 	
 	@Test
-	public void testGetSits() {
-		assertEquals(1, airplane.getSits());
+	public void testGetSeats() {
+		assertEquals(1, airplane.getSeats());
 	}
 	
 	@Test
-	public void testSetSits() {
-		airplane.setSits(2);
-		assertEquals(2, airplane.getSits());
+	public void testSetSeats() {
+		airplane.setSeats(2);
+		assertEquals(2, airplane.getSeats());
 	}
 	
 	@Test
@@ -76,19 +93,28 @@ public class AirplaneTest {
 	}
 	
 	@Test
-	public void testAddFlight() {
-		assertEquals("a Flight is added", airplane.addFlight());
-	}
-	
-	@Test
 	public void testGetAirline() {
 		assertEquals(airline, airplane.getAirline());
 	}
 	
 	@Test
-	public void testSetAirline() {
-		
+	public void testSetAirline() {		
 		airplane.setAirline(airline2);
 		assertEquals(airline2, airplane.getAirline());
+		assertThrows(NullPointerException.class, () -> {airplane.setAirline(null);});
+	}
+	
+	@Test
+	public void testGetFlightList() {
+		assertEquals(flightList, airplane.getFlightList());
+	}
+	
+	@Test
+	public void testSetFlightList() {
+		ArrayList<Flight> newFlightList = new ArrayList<>();
+		newFlightList.add(flight1);
+		airplane.setFlightList(newFlightList);
+		assertEquals(newFlightList, airplane.getFlightList());
+		assertThrows(NullPointerException.class, () -> {airplane.setFlightList(null);});
 	}
 }
