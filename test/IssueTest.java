4cd7b9df5c08721feabe098e7d29e4e258b27dec
s19414 <61875290+s19414@users.noreply.github.com>
2021-12-19 21:53:09 +0100

    Add files via upload

--- test/IssueTest.java
+++ test/IssueTest.java
@@ -1,12 +1,92 @@
 package test;
 
+import static org.junit.Assert.assertEquals;
 import org.junit.Before;
+import org.junit.Test;
+
+import main.Issue;
+import main.Manager;
 
 public class IssueTest {
+	Issue issue;
+	Manager[] managers;
+	Manager extraManager;
 	
 	@Before
 	public void setUp() {
+		issue = new Issue("Terrorist Attack", "Someone flew a bomb-filled jet-powered golf cart into the control tower.", Issue.Severity.HIGH);
+		managers = new Manager[3];
+		managers[0] = new Manager("Ting Wong", "Sum");
+		managers[1] = new Manager("Jiggle", "Physics");
+		managers[2] = new Manager("Theodore", "Herztl");
+		extraManager = new Manager("Tinder", "Box");
+		issue.setManager(managers.clone());
 		
 	}
-
+	
+	@Test
+	public void testNotifyManager() {
+		assertEquals("Manager, notified. Horse, eaten.", issue.notifyManager());
+	}
+	
+	@Test
+	public void testGetManager() {
+		for(int i = 0; i < issue.getManager().length; i++) {
+			assertEquals(managers[i], issue.getManager()[i]);
+		}
+	}
+	
+	@Test
+	public void testSetManager() {
+		managers[0] = extraManager;
+		issue.setManager(managers.clone());
+		for(int i = 0; i < issue.getManager().length; i++) {
+			assertEquals(managers[i], issue.getManager()[i]);
+		}
+	}
+	
+	@Test
+	public void testGetName() {
+		assertEquals("Terrorist Attack", issue.getName());
+	}
+	
+	@Test
+	public void testSetName() {
+		issue.setName("Just a prank, bro.");
+		assertEquals("Just a prank, bro.", issue.getName());
+	}
+	
+	@Test
+	public void testGetInfo() {
+		assertEquals("Someone flew a bomb-filled jet-powered golf cart into the control tower.", issue.getInfo());
+	}
+	
+	@Test
+	public void testSetInfo() {
+		issue.setInfo(issue.getInfo() + " No harm done, he was just kidding around. We need about 10 ambulances though.");
+		assertEquals("Someone flew a bomb-filled jet-powered golf cart into the control tower. No harm done, he was just kidding around. We need about 10 ambulances though.",
+				issue.getInfo());
+	}
+	
+	@Test
+	public void testIsResolved() {
+		assertEquals(false, issue.isResolved());
+	}
+	
+	@Test
+	public void testSetResolved() {
+		issue.setResolved(true);
+		assertEquals(true, issue.isResolved());
+	}
+	
+	@Test
+	public void testGetSeverity() {
+		assertEquals(Issue.Severity.HIGH, issue.getSeverity());
+	}
+	
+	@Test
+	public void testSetSeverity() {
+		issue.setSeverity(Issue.Severity.LOW);
+		assertEquals(Issue.Severity.LOW, issue.getSeverity());
+	}
 }
--- test/LuggageTest.java
+++ test/LuggageTest.java
@@ -1,12 +1,112 @@
 package test;
 
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertThrows;
+
 import org.junit.Before;
+import org.junit.Test;
+
+import main.Luggage;
+import main.Passenger;
 
 public class LuggageTest {
 	
+	Luggage luggage;
+	Passenger passenger;
+	Passenger passenger2;
+	
 	@Before
 	public void setUp() {
+		passenger = new Passenger("Herp", "Derp");
+		passenger2 = new Passenger("Dorpy", "Dorp");
+		luggage = new Luggage(1, Luggage.Category.HAND, 7, 5, passenger);
 		
 	}
+	
+	//test constructor exceptions
+	@Test
+	public void testConstructorCategoryNullException() {
+		assertThrows(NullPointerException.class, () -> {new Luggage(2, null, 2, 2, passenger);});
+	}
 
+	@Test
+	public void testConstructorPassengerNullException() {
+		assertThrows(NullPointerException.class, () -> {new Luggage(2, Luggage.Category.HAND, 2, 2, null);});
+	}
+	
+	//method tests
+	@Test
+	public void testGetIdLuggage() {
+		assertEquals(1, luggage.getIdLuggage());
+	}
+	
+	@Test
+	public void testSetIdLuggage() {
+		luggage.setIdLuggage(4);
+		assertEquals(4, luggage.getIdLuggage());
+	}
+	
+	@Test
+	public void testGetCategory() {
+		assertEquals(Luggage.Category.HAND, luggage.getCategory());
+	}
+	
+	@Test
+	public void testSetCategory() {
+		luggage.setCategory(Luggage.Category.HOLD);
+		assertEquals(Luggage.Category.HOLD, luggage.getCategory());
+		assertThrows(NullPointerException.class, () -> {luggage.setCategory(null);});
+	}
+	
+	@Test
+	public void testGetWeight() {
+		assertEquals(7, luggage.getWeight(), 0.0);
+	}
+	
+	@Test
+	public void testSetWeight() {
+		luggage.setWeight(200);
+		assertEquals(200, luggage.getWeight(), 0.0);
+	}
+	
+	@Test
+	public void testGetPrice() {
+		assertEquals(5, luggage.getPrice(), 0.0);
+	}
+	
+	@Test
+	public void testSetPrice() {
+		luggage.setPrice(80);
+		assertEquals(80, luggage.getPrice(), 0.0);
+	}
+	
+	@Test
+	public void testGetPassenger() {
+		assertEquals(passenger, luggage.getPassenger());
+	}
+	
+	@Test
+	public void testSetPassenger() {
+		luggage.setPassenger(passenger2);
+		assertEquals(passenger2, luggage.getPassenger());
+	}
+	
+	@Test
+	public void testSetPassengerException() {
+		assertThrows(NullPointerException.class, () -> {luggage.setPassenger(null);});
+	}
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
