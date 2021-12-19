b9c9cbb190c09cc6603cc581bb6be62419a4ed7f
s19119 <80397351+s19119@users.noreply.github.com>
2021-12-19 13:59:12 +0100

    Update Flight.java

--- main/Flight.java
+++ main/Flight.java
@@ -23,25 +23,40 @@ public class Flight {
 	//derived attributes
 	public int soldSeats;
 	private LocalTime delay = LocalTime.MIN;
-	private double actualKgOnBoard = 0.0;
+	private double actualKgOnBoard;
 	
 	//temporary attributes
 	private String flightInformation = "Some flight information";
 	
 	public Flight(String _idFlight, Date _estimatedDateOfDeparture,
 			Date _actualDateOfDeparture, Date _estimatedDateOfArrival, Date _actualDateOfArrival, String _destinationFrom,
-			String _destinationTo, int _limitPerHandLuggage) {
+			String _destinationTo, int _limitPerHandLuggage, Airplane _airplane) {
+		if(_idFlight == null) {
+			throw new NullPointerException("Id of flight cannot be null");
+		}
 		idFlight = _idFlight;
 		estimatedDateOfDeparture = _estimatedDateOfDeparture;
 		actualDateOfDeparture = _actualDateOfDeparture;
 		estimatedDateOfArrival = _estimatedDateOfArrival;
 		actualDateOfArrival = _actualDateOfArrival;
+		if(_destinationFrom == null) {
+			throw new NullPointerException("DestinationFrom cannot be null");
+		}
 		destinationFrom = _destinationFrom;
+		if(_destinationTo == null) {
+			throw new NullPointerException("DestinationTo cannot be null");
+		}
 		destinationTo = _destinationTo;
 		limitPerHandLuggage = _limitPerHandLuggage;
-		//TODO derived properties
+		if(_airplane == null) {
+			throw new NullPointerException("Airplane cannot be null");
+		}
+		airplane = _airplane;
+		calculateSoldSeats();
+		calculateDelay();
+		calculateKgOnBoard();
 	}
-	
+
 	public String viewInformationAboutAllFlights() {
 		return flightInformation;
 	}
@@ -51,45 +66,125 @@ public class Flight {
 	}
 	
 	//getters and setters
-	public Airplane getAirplane() {
-		return airplane;
-	}
-	public void setAirplane(Airplane airplane) {
-		this.airplane = airplane;
-	}
-
-	public ArrayList<CheckIn> getCheckInList() {
-		return checkInList;
-	}
-
-	public void setCheckInList(ArrayList<CheckIn> checkInList) {
-		this.checkInList = checkInList;
-		//update actualKgOnBoard as passengers check in
-		actualKgOnBoard = 0;
-		for (CheckIn checkIn : checkInList) {
-			actualKgOnBoard += checkIn.getHandLuggageWeight();
+	public String getIdFlight() {
+		return idFlight;
+	}	
+	public void setIdFlight(String _idFlight) {
+		if(_idFlight == null) {
+			throw new NullPointerException("Id of flight cannot be null");
 		}
+		idFlight = _idFlight;
+	}	
+	public Date getEstimatedDateOfDeparture() {
+		return estimatedDateOfDeparture;
 	}
-
-	public ArrayList<Ticket> getTicketList() {
-		return ticketList;
+	public void setEstimatedDateOfDeparture(Date _estimatedDateOfDeparture) {
+		estimatedDateOfDeparture = _estimatedDateOfDeparture;
 	}
-
-	public void setTicketList(ArrayList<Ticket> ticketList) {
-		this.ticketList = ticketList;
-		soldSeats = ticketList.size();
+	public Date getActualDateOfDeparture() {
+		return actualDateOfDeparture;
 	}
-
+	public void setActualDateOfDeparture(Date _actualDateOfDeparture) {
+		actualDateOfDeparture = _actualDateOfDeparture;
+	}
+	public Date getEstimatedDateOfArrival() {
+		return estimatedDateOfArrival;
+	}
+	public void setEstimatedDateOfArrival(Date _estimatedDateOfArrival) {
+		estimatedDateOfArrival = _estimatedDateOfArrival;
+	}
+	public Date getActualDateOfArrival() {
+		return actualDateOfArrival;
+	}
+	public void setActualDateOfArrival(Date _actualDateOfArrival) {
+		actualDateOfArrival = _actualDateOfArrival;
+	}
+	public String getDestinationFrom() {
+		return destinationFrom;
+	}
+	public void setDestinationFrom(String _destinationFrom) {
+		if(_destinationFrom == null) {
+			throw new NullPointerException("DestinationFrom cannot be null");
+		}
+		destinationFrom = _destinationFrom;
+	}
+	public String getDestinationTo() {
+		return destinationTo;
+	}
+	public void setDestinationTo(String _destinationTo) {
+		if(_destinationTo == null) {
+			throw new NullPointerException("DestinationTo cannot be null");
+		}
+		destinationTo = _destinationTo;
+	}
+	public int getLimitPerHandLuggage() {
+		return limitPerHandLuggage;
+	}
+	public void setLimitPerHandLuggage(int newLimit) {
+		limitPerHandLuggage = newLimit;
+	}
 	public LocalTime getDelay() {
 		return delay;
 	}
-
-	public void setDelay(LocalTime delay) {
-		this.delay = delay;
+	public int getSoldSeats() {
+		return soldSeats;
 	}
-
 	public double getActualKgOnBoard() {
 		return actualKgOnBoard;
 	}
+	public Airplane getAirplane() {
+		return airplane;
+	}
+	public void setAirplane(Airplane _airplane) {
+		if(_airplane == null) {
+			throw new NullPointerException("Airplane cannot be null");
+		}
+		airplane = _airplane;
+	}
+	public ArrayList<CheckIn> getCheckInList(){
+		ArrayList<CheckIn> copyList = new ArrayList<>(checkInList);
+		return copyList;
+	}
+	public void setCheckInList(ArrayList<CheckIn> newCheckIn) {
+		if(newCheckIn == null) {
+			throw new NullPointerException("Check in list cannot be null");
+		}
+		checkInList = new ArrayList<>(newCheckIn);
+	}
+	public ArrayList<Ticket> getTicketList(){
+		ArrayList<Ticket> copyList = new ArrayList<>(ticketList);
+		return copyList;
+	}
+	public void setTicketList(ArrayList<Ticket> newTicketList) {
+		if(newTicketList == null) {
+			throw new NullPointerException("Ticket list cannot be null");
+		}
+		ticketList = new ArrayList<>(newTicketList);
+	}
 	
+	private void calculateDelay() {
+		if(estimatedDateOfArrival != null && actualDateOfArrival != null) {
+			long estimated = estimatedDateOfArrival.getTime();
+			long actual = actualDateOfArrival.getTime();
+			long delayValueInMiliseconds = actual - estimated;
+			delay = LocalTime.ofSecondOfDay(delayValueInMiliseconds/1000);	
+		}
+		else {
+			delay = LocalTime.now();
+		}
+	}
+
+	private void calculateSoldSeats() {
+		soldSeats = ticketList.size();
+	}
+
+	private void calculateKgOnBoard() {
+		actualKgOnBoard = 0;
+		for(Ticket ticket : ticketList) {
+			ArrayList<Luggage> luggageList = ticket.getPassenger().getLuggages();
+			for(Luggage luggage : luggageList) {
+				actualKgOnBoard+=luggage.getWeight();
+			}
+		}
+	}
 }
