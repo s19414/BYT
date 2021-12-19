2e273683ff7ae865fc738d789ca9fb197240bded
s19119 <80397351+s19119@users.noreply.github.com>
2021-12-19 13:19:29 +0100

    Update EmployeeTest.java

--- test/EmployeeTest.java
+++ test/EmployeeTest.java
@@ -1,12 +1,46 @@
 package test;
 
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertThrows;
+
 import org.junit.Before;
+import org.junit.Test;
+
+import main.Employee;
 
 public class EmployeeTest {
 	
+	Employee employee;
+	
 	@Before
 	public void setUp() {
-		
+		employee = new Employee("Steve", "Works");
+	}
+	
+	@Test
+	public void testGetFirstName() {
+		assertEquals("Steve", employee.getFirstName());
+	}
+	
+	@Test
+	public void testSetFirstName() {
+		employee.setFirstName("Mark");
+		assertEquals("Mark", employee.getFirstName());
+		assertThrows(NullPointerException.class, () -> {employee.setFirstName(null);});
+		assertThrows(IllegalArgumentException.class, () -> {employee.setFirstName("123");});
+	}
+	
+	@Test
+	public void testGetLastName() {
+		assertEquals("Works", employee.getLastName());
+	}
+	
+	@Test
+	public void testSetLastName() {
+		employee.setLastName("Byron");
+		assertEquals("Byron", employee.getLastName());
+		assertThrows(NullPointerException.class, () -> {employee.setLastName(null);});
+		assertThrows(IllegalArgumentException.class, () -> {employee.setLastName("Mark12367");});
 	}
 
 }
